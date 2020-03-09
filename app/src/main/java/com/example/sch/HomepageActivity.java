d80b5a12dee8f534ce4a93cca6ec57d3faf6eb1f
Mohamad Chalhoub <mchalhoub42@gmail.com>
2020-03-09 03:40:51 +0200

    Changing the app desgin and logo with some minor changes.

--- app/src/main/java/com/example/sch/HomepageActivity.java
+++ app/src/main/java/com/example/sch/HomepageActivity.java
@@ -4,6 +4,7 @@ import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Typeface;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +12,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextClock;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,27 +26,31 @@ public class HomepageActivity extends AppCompatActivity {
 
     SQLiteDatabase dbReadable;
     ImageButton dosage;
-    Button regstration,reset;
-    TextView dateoftheday;
+    ImageButton regstration, reset;
     TextView patient;
-    String register_status,reset_status;
+    String register_status, reset_status;
+    TextClock textClock1,textClock2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
 
-        dateoftheday=findViewById(R.id.dateoftheday);
-        regstration=findViewById(R.id.register);
-        reset=findViewById(R.id.reset);
-        patient=findViewById(R.id.user);
-        dosage=findViewById(R.id.dosage);
+        regstration = findViewById(R.id.register);
+        reset = findViewById(R.id.reset);
+        patient = findViewById(R.id.user);
+        dosage = findViewById(R.id.dosage);
+        textClock1 = findViewById(R.id.textClock1);
+        textClock2 = findViewById(R.id.textClock2);
 
-
+        reset.setEnabled(false);
+        Typeface type = Typeface.createFromAsset(getAssets(),"alarmclock.ttf");
+        textClock1.setTypeface(type);
+        textClock2.setTypeface(type);
 
         String output = null;
         DBHandler helper = new DBHandler(this);
         dbReadable = helper.getReadableDatabase();
-        SQLiteDatabase dbWritable=helper.getWritableDatabase();
+        SQLiteDatabase dbWritable = helper.getWritableDatabase();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fabhome);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -62,16 +68,12 @@ public class HomepageActivity extends AppCompatActivity {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        Cursor cursor=dbReadable.rawQuery("select * from PATIENT where deleted=0 ",null);
+        Cursor cursor = dbReadable.rawQuery("select * from PATIENT where deleted=0 ", null);
         if (cursor.moveToFirst()) {
             do {
-                patient.setText("Patient name: "+String.valueOf(cursor.getString(cursor.getColumnIndex("name"))));
-            } while (cursor.moveToNext());}
-        DateFormat df = new SimpleDateFormat("EEE, d MMM yyyy, HH:mm");
-        String date = df.format(Calendar.getInstance().getTime());
-        dateoftheday.setText(date);
-
-
+                patient.setText("Hello " + String.valueOf(cursor.getString(cursor.getColumnIndex("name"))).toUpperCase());
+            } while (cursor.moveToNext());
+        }
 
 
         // buttons listener
@@ -79,14 +81,14 @@ public class HomepageActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                Intent i=new Intent(v.getContext(),MainActivity.class);
+                Intent i = new Intent(v.getContext(), MainActivity.class);
                 startActivity(i);
             }
         });
         regstration.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i=new Intent(v.getContext(),RegisterActivity.class);
+                Intent i = new Intent(v.getContext(), RegisterActivity.class);
 
                 startActivity(i);
             }
@@ -94,14 +96,14 @@ public class HomepageActivity extends AppCompatActivity {
         reset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i=new Intent(v.getContext(),RegisterActivity.class);
+                Intent i = new Intent(v.getContext(), RegisterActivity.class);
 
                 startActivity(i);
             }
         });
     }
 
-    public void initialise(){
+    public void initialise() {
         DBHandler helper = new DBHandler(this);
         SQLiteDatabase dbWritable = helper.getWritableDatabase();
 
@@ -151,7 +153,8 @@ public class HomepageActivity extends AppCompatActivity {
         //checking if any patient is registered
         ButtonStatus();
     }
-    private void ButtonStatus(){
+
+    private void ButtonStatus() {
         DBHandler helper = new DBHandler(this);
         SQLiteDatabase dbWritable = helper.getWritableDatabase();
         //buttons stats
@@ -161,22 +164,21 @@ public class HomepageActivity extends AppCompatActivity {
         values.put("resetstatus", "Diasble");
         long result = dbWritable.insert("BUTTONS", null, values);
 
-        Cursor cursor=dbReadable.rawQuery("select * from BUTTONS where id=1",null);
+        Cursor cursor = dbReadable.rawQuery("select * from BUTTONS where id=1", null);
         if (cursor.moveToFirst()) {
             do {
                 //Toast.makeText(this,"ouput : "+cursor.getString(cursor.getColumnIndex("registerstatus")),Toast.LENGTH_LONG).show();
-                register_status=String.valueOf(cursor.getString(cursor.getColumnIndex("registerstatus")));
-                reset_status=String.valueOf(cursor.getString(cursor.getColumnIndex("resetstatus")));
+                register_status = String.valueOf(cursor.getString(cursor.getColumnIndex("registerstatus")));
+                reset_status = String.valueOf(cursor.getString(cursor.getColumnIndex("resetstatus")));
 //            register_status="Enable";
             } while (cursor.moveToNext());
-        }
-        else
-            Toast.makeText(this,"5onfooshari :",Toast.LENGTH_LONG).show();
-        if(reset_status.equals("Enable"))
+        } else
+            Toast.makeText(this, "5onfooshari :", Toast.LENGTH_LONG).show();
+        if (reset_status.equals("Enable"))
             reset.setEnabled(true);
         else
             reset.setEnabled(false);
-        if(register_status.equals("Enable"))
+        if (register_status.equals("Enable"))
             regstration.setEnabled(true);
         else
             regstration.setEnabled(false);
--- app/src/main/java/com/example/sch/RegisterActivity.java
+++ app/src/main/java/com/example/sch/RegisterActivity.java
@@ -21,13 +21,14 @@ import com.example.sch.Databases.DBHandler;
 public class RegisterActivity extends AppCompatActivity {
 
     ImageButton backhome;
-    Button register,reset;
-    EditText name,bloodgrp,bday;
+    Button register, reset;
+    EditText name, bloodgrp, bday;
     RadioGroup gender;
-    RadioButton male,female;
-    String genre,register_status,reset_status;
+    RadioButton male, female;
+    String genre, register_status, reset_status;
 
     SQLiteDatabase dbWritable;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,31 +37,32 @@ public class RegisterActivity extends AppCompatActivity {
 
         final SQLiteDatabase dbReadable = helper.getReadableDatabase();
         dbWritable = helper.getWritableDatabase();
-        name=findViewById(R.id.name);
-        bloodgrp=findViewById(R.id.bloodgp);
-        bday=findViewById(R.id.birth);
-        gender=findViewById(R.id.gender);
-        backhome=findViewById(R.id.backhome);
-        register=findViewById(R.id.register);
-        reset=findViewById(R.id.reset);
-        female=findViewById(R.id.female);
-        male=findViewById(R.id.male);
-        Cursor cursor=dbReadable.rawQuery("select * from BUTTONS where id=1",null);
+
+        name = findViewById(R.id.name);
+        bloodgrp = findViewById(R.id.bloodgp);
+        bday = findViewById(R.id.birth);
+        gender = findViewById(R.id.gender);
+
+        register = findViewById(R.id.register);
+        reset = findViewById(R.id.reset);
+        female = findViewById(R.id.female);
+        male = findViewById(R.id.male);
+
+        Cursor cursor = dbReadable.rawQuery("select * from BUTTONS where id=1", null);
         if (cursor.moveToFirst()) {
             do {
-                Toast.makeText(this,"ouput : "+cursor.getString(cursor.getColumnIndex("registerstatus")),Toast.LENGTH_LONG).show();
-                register_status=String.valueOf(cursor.getString(cursor.getColumnIndex("registerstatus")));
-                reset_status=String.valueOf(cursor.getString(cursor.getColumnIndex("resetstatus")));
+                //Toast.makeText(this,"ouput : "+cursor.getString(cursor.getColumnIndex("registerstatus")),Toast.LENGTH_LONG).show();
+                register_status = String.valueOf(cursor.getString(cursor.getColumnIndex("registerstatus")));
+                reset_status = String.valueOf(cursor.getString(cursor.getColumnIndex("resetstatus")));
 //            register_status="Enable";
             } while (cursor.moveToNext());
-        }
-        else
-            Toast.makeText(this,"5onfooshari :",Toast.LENGTH_LONG).show();
-        if(reset_status.equals("Enable"))
+        } else
+            Toast.makeText(this, "5onfooshari :", Toast.LENGTH_LONG).show();
+        if (reset_status.equals("Enable"))
             reset.setEnabled(true);
         else
             reset.setEnabled(false);
-        if(register_status.equals("Enable"))
+        if (register_status.equals("Enable"))
             register.setEnabled(true);
         else
             register.setEnabled(false);
@@ -69,10 +71,10 @@ public class RegisterActivity extends AppCompatActivity {
         gender.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-                if(male.isChecked())
-                    genre="male";
-                else if(female.isChecked())
-                    genre="female";
+                if (male.isChecked())
+                    genre = "male";
+                else if (female.isChecked())
+                    genre = "female";
             }
         });
         register.setOnClickListener(new View.OnClickListener() {
@@ -81,25 +83,25 @@ public class RegisterActivity extends AppCompatActivity {
 
                 ContentValues values = new ContentValues();
                 values.put("name", String.valueOf(name.getText()));
-                values.put("age",Integer.parseInt(String.valueOf(bday.getText())));
-                values.put("blood_grp",String.valueOf(bloodgrp.getText()));
-                values.put("gender",genre);
-                values.put("deleted",0);
-                long result =  dbWritable.insert("PATIENT", null, values);
+                values.put("age", Integer.parseInt(String.valueOf(bday.getText())));
+                values.put("blood_grp", String.valueOf(bloodgrp.getText()));
+                values.put("gender", genre);
+                values.put("deleted", 0);
+                long result = dbWritable.insert("PATIENT", null, values);
                 values.clear();
                 values.put("registerstatus", "Disable");
                 values.put("resetstatus", "Enable");
-                dbWritable.update("BUTTONS",values,"id=1",null);
-                if (result>0)
+                dbWritable.update("BUTTONS", values, "id=1", null);
+                /*if (result>0)
                     Toast.makeText(v.getContext(),"register done",Toast.LENGTH_LONG).show();
-                Toast.makeText(v.getContext(),"end registration  "+String.valueOf(result),Toast.LENGTH_LONG).show();
+                Toast.makeText(v.getContext(),"end registration  "+String.valueOf(result),Toast.LENGTH_LONG).show();*/
 
                 register.setEnabled(false);
                 reset.setEnabled(true);
 
 
-                Intent i=new Intent(v.getContext(),HomepageActivity.class);
-                i.putExtra("checking","on");
+                Intent i = new Intent(v.getContext(), HomepageActivity.class);
+                i.putExtra("checking", "on");
                 startActivity(i);
             }
         });
@@ -114,32 +116,21 @@ public class RegisterActivity extends AppCompatActivity {
                             public void onClick(DialogInterface dialog, int which) {
                                 ContentValues values = new ContentValues();
                                 values.put("deleted", 1);
-                                dbWritable.update("PATIENT",values,"deleted=0",null);
+                                dbWritable.update("PATIENT", values, "deleted=0", null);
                                 values.clear();
                                 values.put("registerstatus", "Enable");
                                 values.put("resetstatus", "Disable");
-                                dbWritable.update("BUTTONS",values,"id=1",null);
-                                Intent i=new Intent(getApplicationContext(),HomepageActivity.class);
-                                i.putExtra("checking","on");
+                                dbWritable.update("BUTTONS", values, "id=1", null);
+                                Intent i = new Intent(getApplicationContext(), HomepageActivity.class);
+                                i.putExtra("checking", "on");
                                 startActivity(i);
 
 
                             }
                         })
-                        .setNegativeButton(android.R.string.no,null)
+                        .setNegativeButton(android.R.string.no, null)
                         .setIcon(android.R.drawable.ic_dialog_alert)
                         .show();
-
-
-
-
-            }
-        });
-        backhome.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i=new Intent(v.getContext(),HomepageActivity.class);
-                startActivity(i);
             }
         });
     }
